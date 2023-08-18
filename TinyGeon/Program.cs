62662cb8aa91eaf4bdfacbe9fd41f3e9cf21db1a
Atlanticity91 <Atlanticity91@gmail.com>
2023-08-18 20:19:39 +0200

    Ajoutez des fichiers projet.

---
+++ TinyGeon/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new TinyGeon.TinyGame();
+game.Run( );
---
+++ TinyGeon/TinyGame.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TinyGeon {
+
+    public class TinyGame : Game {
+
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        public TinyGame( ) {
+            _graphics             = new GraphicsDeviceManager( this );
+            Content.RootDirectory = "Content";
+            IsMouseVisible        = true;
+
+            _graphics.PreferredBackBufferWidth  = 1280;
+            _graphics.PreferredBackBufferHeight = 720;
+        }
+
+        protected override void Initialize( ) {
+            base.Initialize( );
+        }
+
+        protected override void LoadContent( ) {
+            _spriteBatch = new SpriteBatch( GraphicsDevice );
+        }
+
+        protected override void Update( GameTime gameTime ) {
+        }
+
+        protected override void Draw( GameTime gameTime ) {
+            GraphicsDevice.Clear( Color.CornflowerBlue );
+        }
+
+    }
+
+}
